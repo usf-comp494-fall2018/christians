209b251e16c5714f86d4306cf3e0e63cd11723fe
Christian Serrato <Christian_2223@hotmail.com>
2018-09-10 20:54:01 -0500

    Added Assignment #2 Waldo Program

---
+++ WaldoProgram/WaldoProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WaldoProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Tom", "Bob", "Dana", "Julie", "Sarah", "Fred", "Waldo", "Jenny", "Cathy" };
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i] == "Waldo")
+                {
+                    Console.WriteLine($"Waldo was found at position {i}");
+                    Environment.Exit(0);
+                }
+            }
+
+        }
+    }
+}
