771c707ca56c1c3508691904c3547229bcd12ccb
Christian Serrato <christianserrato@stfrancis.edu>
2018-10-22 08:15:10 -0500

    Finished Midterm Project

---
+++ Midterm Project/CalculatorLib/Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace CalculatorLib
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        //returns mean of array
+        public double Mean(double[] arr)
+        {
+            double sum = 0;
+            int size = arr.Length;
+
+            foreach(double i in arr)
+            {
+                sum = sum + i;
+            }
+
+            return sum / size;
+        }
+
+        //returns median of array
+        public double Median(double[] arr)
+        {
+            int size = arr.Length;
+
+            Array.Sort(arr);
+
+            //if size of array is even, return mean of two middle terms
+            if (size % 2 == 0)
+                return (arr[(size / 2) - 1] + arr[(size / 2)]) / 2;
+            //if size of array is odd, return middle term
+            else
+                return (arr[(size - 1) / 2]);
+        }
+    }
+}
---
+++ Midterm Project/Midterm Project/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using CalculatorLib;
+
+namespace Midterm_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDir = Environment.CurrentDirectory;
+            string input = Path.Combine(currentDir, "CalcInput.txt");
+            string output = Path.Combine(currentDir, "CalcOutput.txt");
+
+            //checks if input file exists, if not, exits application
+            StreamReader textReader;
+            if (!File.Exists(input))
+            {
+                Console.WriteLine("CalcInput.txt does not exist");
+                System.Environment.Exit(1);
+            }
+
+            textReader = File.OpenText(input);
+            //counting number of lines in input file to determine array size
+            var lineCount = 0;
+            using (textReader)
+            {
+                while (textReader.ReadLine() != null)
+                    lineCount++;
+            }
+
+            double[] nums = new double[lineCount];
+
+            //reading in file into array
+            textReader = File.OpenText(input);
+            string line;
+            int i = 0;
+            while ((line = textReader.ReadLine()) != null)
+            {
+                nums[i] = Convert.ToDouble(line);
+                i++;
+            }
+            textReader.Close();
+
+            //creating output file
+            StreamWriter textWriter = File.CreateText(output);
+
+            //calculating and writing results in output file
+            Calculator calc = new Calculator();
+            textWriter.WriteLine("For contents of CalcInput.txt");
+            textWriter.WriteLine($"Mean: {Math.Round(calc.Mean(nums), 3)}");
+            textWriter.WriteLine($"Median: {calc.Median(nums)}");
+            textWriter.Close();
+
+            Console.WriteLine("Output completed and printed in CalcOutput.txt");
+            Console.WriteLine($"Filepath is {output}");
+        }
+    }
+}
